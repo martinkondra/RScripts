6d39b9c0420f2e637165918aed7ecc083927c8b2
Martin Kondra <martinkondra@gmail.com>
2018-08-22 10:59:19 -0300

    k

--- Estadistica08-17.R
+++ Estadistica08-17.R
@@ -14,6 +14,7 @@ anova_insomnio <- aov(PrimerNoche ~ as.factor(Edad), data = insomnio)
 insomnio1 = subset(insomnio, Edad=='Menor a 20 años')
 ks.test(insomnio1$PrimerNoche, "pnorm")
 
+#el p value de schapiro debe ser mayor que alpha
 shapiro.test(insomnio$PrimerNoche)
 
 qqnorm(insomnio_groups$PrimerNoche)
@@ -23,3 +24,27 @@ plot(tuk)
 
 # bonferroni
 # pairwise.t.test(write, ses, p.adj = "bonf")
+
+# EJERCICIO
+setwd("/home/martinkondra/RScripts")
+data <- read.csv("Ej08-17.csv")
+#1
+boxplot(data)
+#2
+stem(data$placebo)
+
+shapiro.test(data$placebo)
+shapiro.test(data$rCognitivo)
+shapiro.test(data$apAcertiva)
+shapiro.test(data$ejYNut)
+
+ks.test(data$placebo, "pnorm")
+ks.test(data$rCognitivo, "pnorm")
+ks.test(data$apAcertiva, "pnorm")
+ks.test(data$ejYNut, "pnorm")
+
+qqnorm(data$placebo)
+
+library(car)
+leveneTest(data$placebo)
+
